ef9f677c8dbd94028adce44b5d8276e1da5e4f2d
Matt <matthewstone218@gmail.com>
2025-07-15 21:33:29 +0900

    ext

--- gm_pymunk/extensions/ext_pymunk/py/physics_engine.py
+++ gm_pymunk/extensions/ext_pymunk/py/physics_engine.py
@@ -6,9 +6,9 @@ test_val = 5
 def set_state(str):
     global test_val
     test_val += 1
-    return "{test_val}".encode()
+    return (f"{test_val}").encode()
 
 def get_state(str):
     global test_val
     test_val += 2
-    return "{test_val}".encode()
+    return (f"{test_val}").encode()
