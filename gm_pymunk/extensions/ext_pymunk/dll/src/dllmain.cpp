131df70a697028149d828f6b8eec8e78a49eae52
Matt <matthewstone218@gmail.com>
2025-07-15 17:48:37 +0900

    ext

---
+++ gm_pymunk/extensions/ext_pymunk/dll/src/dllmain.cpp
@@ -0,0 +1,27 @@
+﻿// dllmain.cpp : DLL 애플리케이션의 진입점을 정의합니다.
+#include "pch.h"
+#include <Windows.h>
+#include <pybind11/embed.h>  // py::scoped_interpreter
+#include <pybind11/pybind11.h>
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+
+namespace py = pybind11;
+
+static bool py_initialized = false;
+static py::object py_module;
